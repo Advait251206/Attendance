03bdfe42a2a505d00028ac9845061713e02e5e7c
Advait Kawale <advaitkawale@gmail.com>
2026-06-13 14:52:25 +0530

    fix: use function for manualChunks to fix typescript error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,8 +8,10 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'framer-motion', 'axios', 'date-fns']
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
         }
       }
     }
